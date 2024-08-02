257911f001ee294592b0ff7b1f4c5ecc33c8be46
Abdugafor Gafarov <76886365+Abdugafor@users.noreply.github.com>
2024-08-02 18:06:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// alert('Выберите продукт \n 1. Себ - 50 рублей \n2. Банан - 70 рублей \n3. Нок - 80 рублей')
+
+// const otvet = prompt(' 1. Себ - 50 рублей \n2. Банан - 70 рублей \n3. Нок - 80 рублей')
+// const kolichestvo = prompt('Напишите количество: ')
+
+// function hisob(price, quantity) {
+//    if (kolichestvo > 20) {
+//         alert('Нестай ика')
+//    }else {
+//     return price * quantity
+//    }
+// }
+
+// if (otvet == "1") {
+//     const totalPrice = hisob(50, kolichestvo )
+//     if (totalPrice != undefined) {
+//         alert(totalPrice)
+//     }
+// }
+// else if (otvet == "2") {
+//     const totalPrice = hisob(70, kolichestvo )
+//     alert(totalPrice)
+// }
+// else if (otvet == "3") {
+//     const totalPrice = hisob(80, kolichestvo )
+//     alert(totalPrice)
+// }
+
+// const rakam = 10 % 2
+
+// for (let i = 0; i < 50; i++) {
+//    console.log()
+// }
+// let ps = prompt('Parol')
+
+// let password = prompt('Потдвериить')
+// while (password != ps) {
+//    password = prompt('')
+// }
+
+// const random = Math.floor(Math.random * 10) + 1
