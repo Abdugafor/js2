2cdd21f92c50ea3830632da4fb3b3202f457069a
Abdugafor Gafarov <76886365+Abdugafor@users.noreply.github.com>
2024-08-05 17:53:55 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// const random = Math.floor(Math.random() * 10) + 1
+// let popitka = 3
+
+// while (popitka > 0) {
+//     let rakamiOdam =  Number(prompt('Введите число от 1 до 10'))
+
+//     if (rakamiOdam === random) {
+//         popitka = 0
+//         alert('Табрик Ефти')
+//     }else {
+//         popitka = popitka - 1
+//         alert('Наефти')
+//     }
+// }
+
+// const list = [7, 2 , 3, 10 , 30]
+// list.length
+// list[0] = 100
+// list[4] = 200
+// console.log(list)
+
+// const list = ['Себ', 'Персик', 'Банан']
+// const reserv = list[0]
+// list[0] = list [2]
+// list[2] = reserv
+// // list.push('ананас')
+// // list.unshift('И аввалдай')
+// // list.pop()
+// // list.shift()
+
+// const list = ['Душанбе', 'Хучанд', 'Москва', 'Cанкт-петербург']
+
+// console.log(list)
